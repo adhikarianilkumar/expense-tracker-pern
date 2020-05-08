d1dea5b1e80b05b33f7d5f5bb584a871d1fbdb43
Anilkumar Adhikari <aadhikar@users.noreply.github.com>
2020-05-08 12:47:51 -0500

    Database connection options

--- server/config/db.js
+++ server/config/db.js
@@ -2,14 +2,33 @@ const { Pool } = require("pg");
 const dotenv = require('dotenv');
 dotenv.config({ path: './config/config.env'});
 
-const pool = new Pool({
+// Programmatic Connection
+const programmaticConnection = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   max: process.env.DB_MAX_CONNECTIONS
-});
+};
+
+// Using Programmatic Connection
+// console.log(`Database Connecting...`.cyan.underline.bold,`
+// Database Host: ${programmaticConnection.host}
+// Database User: ${programmaticConnection.user}
+// Database Port: ${programmaticConnection.port}
+// Database Name: ${programmaticConnection.database}
+// `);
+// const pool = new Pool(programmaticConnection);
+
+// Connection URI
+const connectionUri = {
+  connectionString: process.env.NODE_ENV === 'development' ? process.env.DEVELOPMENT_DB_CONNECTION_URI : process.env.PRODUCTION_DB_CONNECTION_URI
+};
+// Using Connection URI
+console.log(`Database Connecting \nConnection URI ${connectionUri.connectionString}`.cyan.underline.bold);
+const pool = new Pool(connectionUri);
+
 console.log(`Database Connected!`.cyan.underline.bold);
 
 module.exports = pool;
