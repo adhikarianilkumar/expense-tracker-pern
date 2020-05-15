75a6a253ec3ac5db9fbd97a18e53dd41e11be3f2
Anilkumar Adhikari <aadhikar@users.noreply.github.com>
2020-05-15 07:28:33 -0500

    Added queries

--- server/expense_tracker_pern.sql
+++ server/expense_tracker_pern.sql
@@ -19,3 +19,26 @@ values
 	('Pontiac', '89970.12'),
 	('Mitsubishi', '89911.92'),
 	('BMW', '-30367.76');
+
+
+DROP TABLE IF EXISTS user_registration;
+CREATE TABLE user_registration(
+  id uuid DEFAULT uuid_generate_v4(),
+  first_name VARCHAR(255) NOT NULL,
+  middle_name VARCHAR(255),
+  last_name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255),
+  password VARCHAR(255) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+DROP TABLE IF EXISTS user_security_questions;
+CREATE TABLE user_security_questions(
+  id uuid DEFAULT uuid_generate_v4(),
+  security_question VARCHAR(1000) NOT NULL,
+  answer VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  phone_number VARCHAR(255),
+  PRIMARY KEY(id)
+);
